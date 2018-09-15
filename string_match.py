cdc87595b03bbc2e9639bf33eb2c47b1235e5b55
lambert leong <lamberttleong@gmail.com>
2018-09-14 15:52:24 -1000

    writeup underway

--- string_match.py
+++ string_match.py
@@ -7,15 +7,12 @@ from argparse import RawTextHelpFormatter
 from string import ascii_lowercase
 from string import ascii_uppercase
 
-class Agent:
+class Individual:
 	
 	def __init__(self,length):
 		
 		self.string = ''.join(random.choice(string.letters) for _ in xrange(length))
 		self.fitness = -1
-
-	def __str__(self):
-		return 'String: '+str(self.string)+' Fitness: '+str(self.fitness) 
 
 def brute_force():
 	
@@ -23,6 +20,13 @@ def brute_force():
 	
 	for c in in_str:
 		letter = ''
+		stop = False
+		while stop is False:
+			letter = random.choice(string.letters)
+			if letter == c:
+				search_str += letter
+				stop = True
+		"""
 		for d in ascii_lowercase:
 			if c == d:
 				search_str += d
@@ -31,6 +35,7 @@ def brute_force():
 			if c == D:
 				search_str += D
 				break
+		"""
 	if search_str == in_str:
 		print 'FOUND'
 		print 'search_str is ',search_str,' in_str is ',in_str
@@ -43,63 +48,64 @@ def brute_force():
 
 def ga():
 
-	agents = init_agents(population, in_str_len)
-	
+	individuals = init_individuals(population, in_str_len)
+	found = False	
 	for generation in xrange(generations):
 		
-		print 'Generation '+str(generation)
-		
-		agents = fitness(agents)
-		agents = selection(agents)
-		agents = crossover(agents)
-		agents = mutation(agents)
+		generation_list.append(generation)
+		individuals = fitness(individuals)
+		individuals = selection(individuals)
+		individuals = crossover(individuals)
+		individuals = mutation(individuals)
 
-		if any(agent.fitness >= 100 for agent in agents):
-		
-			print 'Threshold met!'
-			exit(0)
+		if any(individual.fitness >= 100 for individual in individuals):
+			found = True	
+			break
+	return found
 
-def init_agents(population, length):
+def init_individuals(population, length):
 
-	return[Agent(length) for _ in xrange(population)]
+	return[Individual(length) for _ in xrange(population)]
 
-def fitness(agents):
+def fitness(individuals):
 	
-	for agent in agents:
+	for individual in individuals:
 	
 		total = len(in_str)*2
 		score = 0
-		for i, letter in enumerate(agent.string):
+		for i, letter in enumerate(individual.string):
 				if in_str[i] == letter:
 					score += 1
 		compare_str = in_str
-		for a_char in agent.string:
+		for a_char in individual.string:
 			for i, in_char in enumerate(compare_str):
 				if a_char == in_char:
 					score += 1
 					compare_str = compare_str[:i]+compare_str[i+1:]
 					break
-		agent.fitness = int((float(score)/float(total))*100)				
+		individual.fitness = int((float(score)/float(total))*100)				
+	return individuals
 
-	return agents
+def selection(individuals):
 
-def selection(agents):
+	individuals = sorted(individuals, key=lambda individual: individual.fitness, reverse=True)
+	
+	max_fit.append(max(individuals, key=lambda individual: individual.fitness).fitness)
+	min_fit.append(min(individuals, key=lambda individual: individual.fitness).fitness)
+	avg_fit.append(float(sum(i.fitness for i in individuals)//len(individuals)))
 
-	agents = sorted(agents, key=lambda agent: agent.fitness, reverse=True)
-#	print '\n'.join(map(str,agents))
-	agents = agents[:int(0.2*len(agents))]
+	individuals = individuals[:int(0.2*len(individuals))]
 
-	return agents
+	return individuals
 
-def crossover(agents):
-#	print 'RAND AGENT IS '+str(random.choice(agents))	
+def crossover(individuals):
 	offspring = []
 	
-	for _ in xrange((population - len(agents))/2):
-		parent1 = random.choice(agents)
-		parent2 = random.choice(agents)
-		child1 = Agent(in_str_len)
-		child2 = Agent(in_str_len)
+	for _ in xrange((population - len(individuals))/2):
+		parent1 = random.choice(individuals)
+		parent2 = random.choice(individuals)
+		child1 = Individual(in_str_len)
+		child2 = Individual(in_str_len)
 		split = random.randint(0, in_str_len)
 		child1.string = parent1.string[0:split] + parent2.string[split:in_str_len]
 		child2.string = parent2.string[0:split] + parent1.string[split:in_str_len]
@@ -107,21 +113,21 @@ def crossover(agents):
 		offspring.append(child1)
 		offspring.append(child2)
 
-	agents.extend(offspring)
+	individuals.extend(offspring)
 
-	return agents
+	return individuals
 
-def mutation(agents):
+def mutation(individuals):
 	
-	for agent in agents:
+	for individual in individuals:
 		
-		for idx, param in enumerate(agent.string):
+		for i, param in enumerate(individual.string):
 
-			if random.uniform(0.0, 1.0) <= 0.1:
+			if random.uniform(0.0, 1.0) <= 0.05:
 			
-				agent.string = agent.string[0:idx] + random.choice(string.letters) + agent.string[idx+1:in_str_len]
+				individual.string = individual.string[0:i] + random.choice(string.letters) + individual.string[i+1:in_str_len]
 
-	return agents
+	return individuals
 
 def parse_arguments():
 
@@ -138,7 +144,12 @@ def parse_arguments():
 in_str = None
 in_str_len = None
 population = 20
-generations = 10000
+generations = 5000
+max_fit = [0]
+avg_fit = [0]
+min_fit = [0]
+generation_list = [0]
+
 
 if __name__== '__main__':
 
@@ -150,7 +161,22 @@ if __name__== '__main__':
 	in_str = argv.in_str
 	in_str_len = len(in_str)
 	if argv.algorithm == 'genetic' or argv.algorithm == 'g':
-		ga()
+		if ga() is True:
+
+			#sys.exit(0)
+			import matplotlib.pyplot as plt
+			import pylab
+			plt.plot(generation_list, max_fit, label = 'Max Fitness')
+			plt.plot(generation_list, avg_fit, label = 'Average Fitness')
+			plt.plot(generation_list, min_fit, label = 'Min Fitness')
+			pylab.legend(loc='lower right')
+			#pylab.ylim(-1.5, 2.0)
+			plt.xlabel('Generation')
+			plt.ylabel('Fitness Score out of 100')
+			plt.title('Genetic Algorithms Performance for Matching String = '+in_str)
+			plt.show()
+			sys.exit(0)
+
 	elif argv.algorithm == 'brute' or argv.algorithm == 'b':
 		brute_force()
 		#print 'to be implemented'
